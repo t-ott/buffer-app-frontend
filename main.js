47265b8bd4b734de30f671ab8ebd97c1db4c300f
Tommy Ott <tommy.ott617@gmail.com>
2023-07-29 14:00:27 -0400

    Add geomType dropdown

--- main.js
+++ main.js
@@ -2,14 +2,55 @@ import './style.css';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+import {Control, defaults as defaultControls} from 'ol/control.js';
+
+class GeomTypeControl extends Control {
+  /**
+   * @param {Object} [opt_options] Control options.
+   */
+  constructor(opt_options) {
+    const options = opt_options || {};
+
+    const form = document.createElement('form');
+    const select = document.createElement('select');
+
+    const optionPoint = document.createElement('option');
+    optionPoint.setAttribute('value', 'Point');
+    optionPoint.innerHTML = 'Point';
+    select.appendChild(optionPoint);
+
+    const optionLineString = document.createElement('option');
+    optionLineString.setAttribute('value', 'LineString');
+    optionLineString.innerHTML = 'LineString';
+    select.appendChild(optionLineString);
+
+    const optionPolygon = document.createElement('option');
+    optionPolygon.setAttribute('value', 'Polygon');
+    optionPolygon.innerHTML = 'Polygon';
+    select.appendChild(optionPolygon);
+
+    form.appendChild(select);
+
+    const element = document.createElement('div');
+    element.className = 'ol-control';
+    element.id = 'geomTypeControl'
+    element.appendChild(form);
+
+    super({
+      element: element,
+      target: options.target,
+    });
+  }
+}
 
 const map = new Map({
-  target: 'map',
+  controls: defaultControls().extend([new GeomTypeControl()]),
   layers: [
     new TileLayer({
       source: new OSM()
     })
   ],
+  target: 'map',
   view: new View({
     center: [0, 0],
     zoom: 2
